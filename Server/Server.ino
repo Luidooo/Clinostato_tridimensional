2c2b64eec9aafa33d27ddf69604089457a554dbe
luid <eng.limaluis@gmail.com>
2024-03-03 13:45:57 -0300

    integration css and html in esp32 webServer

---
+++ Server/Server.ino
@@ -0,0 +1,51 @@
+#include <WiFi.h>
+#include <WebServer.h>
+#include <SPIFFS.h>
+
+const char* ssid = "MENDES";
+const char* password = "Ml2721091201";
+
+WebServer server(80);
+
+void setup() {
+  Serial.begin(115200);
+
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(1000);
+    Serial.println("Connecting to WiFi...");
+  }
+  Serial.println("Connected to WiFi");
+
+  if (!SPIFFS.begin(true)) {
+    Serial.println("Failed to mount SPIFFS file system");
+    return;
+  }
+
+  server.on("/", HTTP_GET, []() {
+    File file = SPIFFS.open("/index.html", "r");
+    if (!file) {
+      Serial.println("Failed to open index.html file");
+      return;
+    }
+    server.streamFile(file, "text/html");
+    file.close();
+  });
+
+  server.on("/styles.css", HTTP_GET, []() {
+    File file = SPIFFS.open("/styles.css", "r");
+    if (!file) {
+      Serial.println("Failed to open styles.css file");
+      return;
+    }
+    server.streamFile(file, "text/css");
+    file.close();
+  });
+
+  server.begin();
+  Serial.println("HTTP server started");
+}
+
+void loop() {
+  server.handleClient();
+}
